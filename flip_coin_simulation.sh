7d495009697ce66521c9f4eaa8a2e5a8ffd55972
yburkul <yburkul123@gmail.com>
2022-02-25 15:49:00 +0530

    Added: Flip coin

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -1,1 +1,15 @@
 #Flip Coin Simulation Problem
+
+#flip a coin to display heads or tails as winner
+
+tail=0
+head=1
+
+result=$((RANDOM%2))
+
+if (($result==1))
+then
+    echo "winner" = $head
+else
+    echo "loser" = $tail
+fi
