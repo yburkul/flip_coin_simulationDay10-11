7b2959f5e38c447434c343cf012ad3bf9ff5d7bf
yburkul <yburkul123@gmail.com>
2022-02-25 15:58:15 +0530

    Added: win head and tail

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -4,12 +4,21 @@
 
 tail=0
 head=1
-
+head_count=0
+tail_count=0
+for(( i=1 ; i<=100 ; i++ ))
+do
 result=$((RANDOM%2))
 
 if (($result==1))
 then
+    ((head_count++))
     echo "winner" = $head
 else
+    ((tail_count++))
     echo "loser" = $tail
 fi
+done
+
+echo "Number of time head win: " $head_count
+echo "Number of time tail win: " $tail_count
